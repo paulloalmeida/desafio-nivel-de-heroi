8c0f1011a55b4b632195093c9e8c2b51a2a331c3
Paulo Almeida <paulloalmeida.84@gmail.com>
2024-11-29 00:10:19 -0300

    update nivel-heroi

--- classificador-de-nivel-de-heroi.js
+++ classificador-de-nivel-de-heroi.js
@@ -1,37 +1,38 @@
 let heroi = "Batman";
 let xp = 4555;
 let nivel;
-switch (true) {
+
+  switch (true) {
     case xp <= 1000:
         nivel = "Ferro";
         console.log("O herói " + heroi + " está no nível " + nivel  + "!");
         break;
     case xp > 1000 & xp <= 2000:
-        nivel = "Bronze"
+        nivel = "Bronze";
         console.log("O herói " + heroi + " está no nível " + nivel  + "!");
         break;
     case xp > 2000 & xp <= 5000:
-        nivel = "Prata"
+        nivel = "Prata";
         console.log("O herói " + heroi + " está no nível " + nivel  + "!");
         break;
     case xp > 5000 & xp <= 7000:
-        nivel = "Ouro"
+        nivel = "Ouro";
         console.log("O herói " + heroi + " está no nível " + nivel  + "!");
         break;
     case xp > 7000 & xp <= 8000:
-        nivel = "Platina"
+        nivel = "Platina";
         console.log("O herói " + heroi + " está no nível " + nivel  + "!");
         break;
     case xp > 8000 & xp <= 9000:
-        nivel = "Ascendente"
+        nivel = "Ascendente";
         console.log("O herói " + heroi + " está no nível " + nivel  + "!");
-        break
+        break;
     case xp > 9000 & xp <= 10000:
-        nivel = "Imortal"
+        nivel = "Imortal";
         console.log("O herói " + heroi + " está no nível " + nivel  + "!");
         break;
     case xp > 10000:
-        nivel = "Radiante"
+        nivel = "Radiante";
         console.log("O herói " + heroi + " está no nível " + nivel  + "!");
         break;
     
